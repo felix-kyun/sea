4f6f2a847b93af860892310305b57152502d8f77
Praise Jacob <iamfelixkyun@gmail.com>
2025-06-17 23:06:48 +0530

    chore(core): update dependencies and add launcher readme

--- install.sh
+++ install.sh
@@ -2,11 +2,18 @@
 
 # set up the deps
 DEPS=(
+    "kitty" # ofc 
+
     "gum" # for all the pretty output
     "upower" # for battery level
+    "fzf" # for fuzzy searching
 
     # core utils
     "grep"
     "awk"
     "tr"
+    "cut"
+    "find"
+    "sed"
+    "setsid"
     )

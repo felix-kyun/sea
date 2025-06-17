44edba9c6916030af47e8587e7bc1d1df2f0e8f6
Praise Jacob <iamfelixkyun@gmail.com>
2025-06-17 21:51:26 +0530

    feat(fetch): add fetch utility

---
+++ fetch/zetch.sh
@@ -0,0 +1,172 @@
+#! /usr/bin/env bash 
+
+# deps 
+# - figlet
+# - toilet ?
+# - tput
+# - awk 
+# - grep 
+# - cut 
+
+# setting ?
+# You want settings ?
+# Look no further, adjust the variables below lmao
+COLUMN=6
+BLACK=0
+RED=1
+GREEN=2
+YELLOW=3
+BLUE=4
+MAGENTA=5
+CYAN=6
+WHITE=7
+DEFAULT=9
+
+show_fonts() {
+  for font in $(ls /usr/share/figlet/fonts | sed 's/\(.*\.flc\|\.flf\)//g')
+  do
+    figlet -f $font $font
+  done
+}
+
+show_logo() {
+  DISTRO_NAME=$(cat /etc/os-release | grep "PRETTY_NAME=" | cut -d "=" -f 2 | sed 's/\("\| .*\)//g')
+  echo $(tput bold) $(tput setaf 4) 
+  figlet -f smslant $DISTRO_NAME
+  echo -n $(tput sgr0)
+}
+
+print_mod() {
+  # format to be used 
+  # print_mod(COLOR, ICON, NAME, VALUE)
+  
+  echo -n $(tput bold)
+  echo -n "│ "
+
+  # print the colored icon 
+  echo -n $(tput setaf $1) 
+  echo -n "${2} "
+  echo -n $(tput setaf $WHITE)
+
+  printf " %-${COLUMN}s" $3
+
+  echo -n " │ "
+  echo -n $(tput setaf $1) 
+  shift 3
+  echo -n "${@}"
+  echo $(tput setaf $DEFAULT)
+  echo -n $(tput sgr0)
+}
+
+dummy_print() {
+  echo "dummy text"
+}
+
+show_top() {
+  echo -n "╭"
+
+  for (( i = 1; i <= ${COLUMN} + 5; i++))
+  do 
+    echo -n "─"
+  done
+
+  echo "╮"
+}
+
+show_bottom() {
+  echo -n "╰"
+
+  for (( i = 1; i <= ${COLUMN} + 5; i++))
+  do 
+    echo -n "─"
+  done
+
+  echo "╯"
+}
+
+show_user() {
+  echo $USER
+}
+
+show_hname() {
+  cat /etc/hostname
+}
+
+show_pretty_disro() {
+  cat /etc/os-release | grep "PRETTY_NAME=" | cut -d "=" -f 2 | sed 's/"//g'
+}
+
+show_kernel() {
+  uname -r
+}
+
+show_shell() {
+  echo $SHELL
+}
+
+show_packages() {
+  if $(command -v apt 1>/dev/null); then 
+    echo $(dpkg -–list | wc -l) "(deb)"
+  elif $(command -v pacman 1>/dev/null); then 
+    echo $(pacman -Q | wc -l) "(pacman)"
+  elif $(command -v dnf 1>/dev/null); then 
+    echo $(dnf list installed | wc -l) "(rpm)"
+  fi
+}
+
+show_uptime() {
+  uptime -p
+}
+
+show_memory() {
+  echo $(free -h | awk '/Mem:/{print $3}') "|" $(free -h | awk '/Mem:/{print $2}')
+}
+
+
+load_async() {
+  ZETCH_TMP=$(mktemp -d)
+
+  show_user > $ZETCH_TMP/zetch_user.tmp &
+  show_hname > $ZETCH_TMP/zetch_hname.tmp &
+  show_pretty_disro > $ZETCH_TMP/zetch_pretty_disro.tmp &
+  # show_kernel > $ZETCH_TMP/zetch_kernel.tmp &
+  # show_shell > $ZETCH_TMP/zetch_shell.tmp &
+  # show_packages > $ZETCH_TMP/zetch_packages.tmp &
+  show_uptime > $ZETCH_TMP/zetch_uptime.tmp &
+  show_memory > $ZETCH_TMP/zetch_memory.tmp &
+
+  # wait for all background jobs to finish
+  wait
+
+  export ZETCH_USER=$(cat $ZETCH_TMP/zetch_user.tmp)
+  export ZETCH_HNAME=$(cat $ZETCH_TMP/zetch_hname.tmp)
+  export ZETCH_PRETTY_DISTR=$(cat $ZETCH_TMP/zetch_pretty_disro.tmp)
+  # export ZETCH_KERNEL=$(cat $ZETCH_TMP/zetch_kernel.tmp)
+  # export ZETCH_SHELL=$(cat $ZETCH_TMP/zetch_shell.tmp)
+  # export ZETCH_PACKAGES=$(cat $ZETCH_TMP/zetch_packages.tmp)
+  export ZETCH_UPTIME=$(cat $ZETCH_TMP/zetch_uptime.tmp)
+  export ZETCH_MEMORY=$(cat $ZETCH_TMP/zetch_memory.tmp)
+}
+
+
+main() {
+  # clear
+  # echo ""
+  # show_logo
+  show_top
+  load_async
+
+  # define modules here
+  print_mod $RED "" "user" $ZETCH_USER
+  print_mod $YELLOW "󰍹" "hname" $ZETCH_HNAME
+  print_mod $GREEN "󰌽" "distro" $ZETCH_PRETTY_DISTR
+  # print_mod $CYAN "" "kernel" $ZETCH_KERNEL
+  print_mod $BLUE "" "uptime" $ZETCH_UPTIME
+  # print_mod $MAGENTA "" "shell" $ZETCH_SHELL
+  # print_mod $RED "󰏔" "pkgs" $ZETCH_PACKAGES
+  print_mod $YELLOW "󰍛" "memory" $ZETCH_MEMORY
+
+  show_bottom
+}
+
+main

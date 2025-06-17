df9f2a6b7e3ff65d4e1be48afc54ad335a46fe0f
Praise Jacob <iamfelixkyun@gmail.com>
2025-06-17 23:07:34 +0530

    refactor(core): seperate out bash text formatting codes into utils

---
+++ utils/formatting.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Hide/Show Cursor
+HIDE_CURSOR="\e[?25l"
+SHOW_CURSOR="\e[?25h"
+
+# text formatting
+RESET="\033[0m"
+BOLD="\033[1m"
+UNDERLINE="\033[4m"
+REVERSED="\033[7m"
+
+# foreground colors
+BLACK="\033[30m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+MAGENTA="\033[35m"
+CYAN="\033[36m"
+WHITE="\033[37m"
+
+# background colors
+BLACK_BG="\033[40m"
+RED_BG="\033[41m"
+GREEN_BG="\033[42m"
+YELLOW_BG="\033[43m"
+BLUE_BG="\033[44m"
+MAGENTA_BG="\033[45m"
+CYAN_BG="\033[46m"
+WHITE_BG="\033[47m"
+

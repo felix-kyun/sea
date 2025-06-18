da994ce46eb74b71575e8ec9787fb4a1344e275b
Praise Jacob <iamfelixkyun@gmail.com>
2025-06-19 04:37:01 +0530

    chore: rename to sea-launcher and increase notification timeout

--- launcher/launcher.sh
+++ launcher/launcher.sh
@@ -3,6 +3,6 @@
 CURRENT_DIR=$(dirname "${BASH_SOURCE[0]}")
 
 kitty \
-    --title sea-shell \
+    --title sea-launcher \
     --config ${CURRENT_DIR}/launcher.conf \
     -- bash -c "source ${CURRENT_DIR}/utils.sh && show_launcher"

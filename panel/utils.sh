0b762459f7de6228d425e3586203d1bdc00ae34a
Praise Jacob <iamfelixkyun@gmail.com>
2025-06-19 04:18:28 +0530

    fix(panel): remove gum log due to garbage osc and cpr sequences produced

--- panel/utils.sh
+++ panel/utils.sh
@@ -1,26 +1,14 @@
 #!/usr/bin/env bash
 
-debug () {
-    if [ "$DEBUG" = true ]; then
-        gum log --level debug "$@"
-    fi
-}
-
-debug_exec() {
-    if [ "$DEBUG" = true ]; then
-        gum log --level debug $("$@")
-    fi
-}
-
 log () {
-    LOG_SOURCE="${BASH_SOURCE[1]}"
-    source $(dirname "${BASH_SOURCE[0]}")/config.sh
-    gum log --level $1 --time Timeonly -o "${LOG_FILE}" "${LOG_SOURCE}: ${@:2}"
-    debug "${1}: ${LOG_SOURCE}: ${@:2}"
+    (
+        LOG_SOURCE="${BASH_SOURCE[1]}"
+        source $(dirname "${BASH_SOURCE[0]}")/config.sh
+        echo "$(date +"%H:%M:%S.%3N") [${1^^}] ${LOG_SOURCE}: ${@:2}" >> "${LOG_FILE}"
+    ) & &>/dev/null
 }
 
 send () {
     echo -e "${PLUGIN_ID}:${1}:${@:2}" | socat - UNIX-CONNECT:"${SOCKET}" 
-    sleep 0.1 # give some time for the socket to process the message
 }
 

1c4fdeb8222aa9091bf9994a3f28c04ac32c95c3
Praise Jacob <iamfelixkyun@gmail.com>
2025-06-19 18:32:04 +0530

    chore(panel): update example plugin spec

---
+++ panel/plugins/example.spec.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# default value before the plugin send update
+example_default="example" 
+
+# runs once when the plugin is loaded
+example_onload() {
+    log info "Example plugin loaded"
+}
+
+# runs when the plugin is started
+# this function must be defined
+example_start() {
+    # important: this line *must* be defined before calling send
+    PLUGIN_ID=$1 
+
+    # example controls
+    send bg "${BLUE_BG}"
+    send fg "${WHITE}"
+    send update "󰒡 Example Plugin"
+
+    while :; do 
+        # Simulate some work
+        sleep 2
+        send update "Example Plugin is running"
+    done
+}
+
+# run when the bar is closed
+example_unload() {
+    log info "Example plugin unloaded"
+}

6f3bd7c0ba6cfa96be5b3b4d65bec1d8b8be5289
Praise Jacob <iamfelixkyun@gmail.com>
2025-06-23 23:24:57 +0530

    refactor(core): refactor mouse event handler functions

--- panel/mouse_handler.sh
+++ panel/mouse_handler.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 
+handler_plugin_mouse_event() {
+    local plugin_id="$1"
+    local event="$2"
+
+    log mouse "${event} on ${plugin_id}"
+
+    [[ "${event}" == "press" ]] \
+        && declare -f "${name}_onclick" &> /dev/null \
+        && "${name}_onclick" "${id}"
+}
+
 handler_left() {
-    x=$1
+    event=$1
+    x=$2
     keep_x=0
     for plugin in "${PLUGIN_LEFT[@]}"; do
         IFS=':' read name id <<< "${plugin}"
         plugin_x_end=$(( keep_x + "${id}_len" + 2 ));
 
         if (( x <= plugin_x_end && x >= keep_x )); then
-            log mouse "clicked on ${name} at ${x}"
-            declare -f "${name}_onclick" &> /dev/null \
-                && "${name}_onclick" "${id}"
-            return 0
+            handler_plugin_mouse_event "${id}" "${event}"
         fi
 
         keep_x=$plugin_x_end
@@ -19,17 +28,15 @@ handler_left() {
 }
 
 handler_center() {
-    x=$1
+    event=$1
+    x=$2
     keep_x=$(( left_len + left_padding_len ))
     for plugin in "${PLUGIN_CENTER[@]}"; do
         IFS=':' read name id <<< "${plugin}"
         plugin_x_end=$(( keep_x + "${id}_len" + 2 ));
 
         if (( x <= plugin_x_end && x >= keep_x )); then
-            log mouse "clicked on ${name} at ${x}"
-            declare -f "${name}_onclick" &> /dev/null \
-                && "${name}_onclick" "${id}"
-            return 0
+            handler_plugin_mouse_event "${plugin}" "${event}"
         fi
 
         keep_x=$plugin_x_end
@@ -37,17 +44,15 @@ handler_center() {
 }
 
 handler_right() {
-    x=$1
+    event=$1
+    x=$2
     keep_x=$(( left_len + left_padding_len + center_len + right_padding_len ))
     for plugin in "${PLUGIN_RIGHT[@]}"; do
         IFS=':' read name id <<< "${plugin}"
         plugin_x_end=$(( keep_x + "${id}_len" + 2 ));
 
         if (( x <= plugin_x_end && x >= keep_x )); then
-            log mouse "clicked on ${name} at ${x}"
-            declare -f "${name}_onclick" &> /dev/null \
-                && "${name}_onclick" "${id}"
-            return 0
+            handler_plugin_mouse_event "${plugin}" "${event}"
         fi
 
         keep_x=$plugin_x_end
@@ -59,19 +64,13 @@ mouse_handler() {
     local event="$1"
     IFS=';' read x y <<< "$2"
 
-    log mouse "${event} at (${x}, ${y})"
-    [[ "${event}" != "press" ]] && return 0;
-
     if (( x >= 0 && x <= left_len )); then
-        log mouse "Left area clicked at ${x}"
-        handler_left "${x}"
+        handler_left "${event}" "${x}"
     elif (( x >= (left_len + left_padding_len) && \
               x <= (left_len + left_padding_len + center_len) )); then
-        log mouse "Center area clicked at ${x}"
-        handler_center "${x}"
+        handler_center "${event}" "${x}"
     elif (( x >= (left_len + left_padding_len + center_len + right_padding_len) && \
               x <= COLS )); then
-        log mouse "Right area clicked at ${x}"
-        handler_right "${x}"
+        handler_right "${event}" "${x}"
     fi
 }

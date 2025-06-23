6162e87ff2a42b4001a677321afaccc186fc1adb
Praise Jacob <iamfelixkyun@gmail.com>
2025-06-23 09:19:27 +0530

    chore(panel:net_speed): lower update delay

--- panel/plugins/net_speed.sh
+++ panel/plugins/net_speed.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
+net_speed_delay=5
+
 net_speed_start() {
     PLUGIN_ID=$1
 
     while :; do 
         read rx_current tx_current <<< $(awk 'NR>2 { rx+=$2; tx+=$10 } END { print rx, tx }' /proc/net/dev)
 
-        sleep 2
+        sleep "${net_speed_delay}"
 
         # store the new network usage
         read rx_new tx_new <<< $(awk 'NR>2 { rx+=$2; tx+=$10 } END { print rx, tx }' /proc/net/dev)
 
-        rx_diff=$(((rx_new - rx_current) / 1024))  
-        tx_diff=$(((tx_new - tx_current) / 1024))  
+        rx_diff=$(((rx_new - rx_current) / (1024 * net_speed_delay)))  
+        tx_diff=$(((tx_new - tx_current) / (1024 * net_speed_delay)))  
 
         send update " ${rx_diff}KiB/s  ${tx_diff}KiB/s"
 

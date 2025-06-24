3a0395e3f5132c22f6db67dc2ee8e1d16ebc76c0
Praise Jacob <iamfelixkyun@gmail.com>
2025-06-25 01:55:56 +0530

    feat(powermenu): add icons for labels

--- powermenu/powermenu.sh
+++ powermenu/powermenu.sh
@@ -3,12 +3,12 @@
 CURRENT_DIR=$(dirname "${BASH_SOURCE[0]}")
 source "${CURRENT_DIR}/../utils/fzf_theme_override.sh"
 actions=(
-    "Power Off:systemctl poweroff"
-    "Reboot:systemctl reboot"
-    "Suspend:systemctl suspend"
-    "Hibernate:systemctl hibernate"
-    "Lock:loginctl lock-session ${XDG_SESSION_ID}"
-    "Logout:loginctl terminate-session ${XDG_SESSION_ID}"
+    " Power Off:systemctl poweroff"
+    "󰜉 Reboot:systemctl reboot"
+    "󰒲 Suspend:systemctl suspend"
+    "󰋊 Hibernate:systemctl hibernate"
+    "󰌾 Lock:loginctl lock-session ${XDG_SESSION_ID}"
+    "󰍃 Logout:loginctl terminate-session ${XDG_SESSION_ID}"
 )
 
 selected=$(printf '%s\n' "${actions[@]}" \

b317e53c7608a5df9e8b23d26bd54d6336c5965c
Praise Jacob <iamfelixkyun@gmail.com>
2025-06-25 00:23:31 +0530

    feat(panel): add a env var to disable logging

--- panel/log.sh
+++ panel/log.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 log () {
+    # disable logging when $LOG set to false
+    [[ $LOG == false ]] && return
+
     (
         source $(dirname "${BASH_SOURCE[0]}")/config.sh
         echo "$(date +"%H:%M:%S.%3N") [${1^^}]: ${@:2}" >> "${LOG_FILE}"

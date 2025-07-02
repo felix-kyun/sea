a4444e5c7c3f50fc938d50adf0e9f30a7ee345fe
Praise Jacob <iamfelixkyun@gmail.com>
2025-07-02 14:24:32 +0530

    fix(panel): fix notification content getting chipped off

--- panel/notification.sh
+++ panel/notification.sh
@@ -20,8 +20,8 @@ show_notification() {
     local buffer main_len main_buffer padding show_buffer padding_len
 
     buffer="\r\033[2K${notify_fg}"
-    main_len=$((${#notification}))
     main_buffer="${notification_icon}${notification}"
+    main_len=${#main_buffer}
     padding=""
 
     delay_clear "${my_id}" &
@@ -29,12 +29,12 @@ show_notification() {
     # show notification animation
     # no animation for osd
     if [[ "$2" == "osd" ]]; then
-        padding_len=$(((COLS - main_len - 2) / 2))
+        padding_len=$(((COLS - ${#notification} - 2) / 2))
         padding=$(pad "${padding_len}")
 
         echo -ne "${buffer}${padding}${notification}${padding}"
     else
-        for ((i = 1; i <= main_len; i += 2)); do
+        for ((i = 0; i <= main_len; i += 2)); do
             show_buffer="${main_buffer::$i}"
             padding_len=$(((COLS - i - 2) / 2))
             padding=$(pad "${padding_len}")

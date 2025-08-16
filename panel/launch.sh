aa4659dc9ffacf73c65cd03a42ef7268da3d0f6d
Praise Jacob <iamfelixkyun@gmail.com>
2025-08-17 01:42:08 +0530

    feat: add config file loading for panel launch and panel_output to set the display on which panel is shown

--- panel/launch.sh
+++ panel/launch.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 CURRENT_DIR=$(dirname "${BASH_SOURCE[0]}")
+[[ -f "$HOME/.config/sea.sh" ]] && source "$HOME/.config/sea.sh"
 
 kitten panel \
     --config "${CURRENT_DIR}/panel.conf" \
@@ -9,5 +10,7 @@ kitten panel \
     --name sea-panel \
     --class sea-panel \
     --lines 1 \
+    --detach \
+    --output-name "${PANEL_OUTPUT}" \
     -- bash -c "while :; do ${CURRENT_DIR}/main.sh; done" \
     &>/tmp/sea-panel-launch.log

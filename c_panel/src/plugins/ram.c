ee254e81b5e853f03e8a76026404aabfdcf20f5f
Praise Jacob <iampraisejacob@gmail.com>
2025-09-06 00:36:53 +0530

    feat(c_panel): add cpu ram plugin

---
+++ c_panel/src/plugins/ram.c
@@ -0,0 +1,78 @@
+#include "colors.h"
+#include "log.h"
+#include "plugins/plugins.h"
+#include "state.h"
+#include "string-utf8.h"
+#include "utils.h"
+#include <stdint.h>
+#include <stdio.h>
+
+#define RAM_ICON "  "
+#define RAM_COLOR MAGENTA
+#define MEM_FILE "/proc/meminfo"
+
+static char buffer[32];
+
+inline static void set_ram_usage(PluginState* state, float used, float total)
+{
+    (void)total;
+    snprintf(buffer, sizeof(buffer), " " RAM_COLOR RAM_ICON "%.1fGib " RESET, used);
+    string_set_cstr(state->data, buffer);
+    panel_signal_render();
+}
+
+uint32_t get_total_ram(void)
+{
+    FILE* file = fopen(MEM_FILE, "r");
+    if (!file) {
+        logger_log(LOG_ERROR, "failed to open meminfo file: " MEM_FILE);
+        return -1;
+    }
+
+    uint32_t total_kb = 0;
+    char tmp_buffer[64];
+    while (fgets(tmp_buffer, sizeof(tmp_buffer), file)) {
+        if (sscanf(tmp_buffer, "MemTotal: %d kB", &total_kb) == 1) {
+            break;
+        }
+    }
+
+    fclose(file);
+    return total_kb;
+}
+
+uint32_t get_available_ram(void)
+{
+    FILE* file = fopen(MEM_FILE, "r");
+    if (!file) {
+        logger_log(LOG_ERROR, "failed to open meminfo file: " MEM_FILE);
+        return -1;
+    }
+
+    uint32_t available_kb = 0;
+    char tmp_buffer[64];
+    while (fgets(tmp_buffer, sizeof(tmp_buffer), file)) {
+        if (sscanf(tmp_buffer, "MemAvailable: %d kB", &available_kb) == 1) {
+            break;
+        }
+    }
+
+    fclose(file);
+    return available_kb;
+}
+
+void* plugin_ram(void* _state)
+{
+    PluginState* state = _state;
+    uint32_t total_ram = get_total_ram();
+
+    while (running) {
+        float used_ram = ((float)total_ram - get_available_ram()) / 1024 / 1024;
+        float total_gb = (float)total_ram / 1024 / 1024;
+
+        set_ram_usage(state, used_ram, total_gb);
+        msleep(5000);
+    }
+
+    return NULL;
+}

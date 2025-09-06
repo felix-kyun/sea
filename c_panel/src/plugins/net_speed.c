5f554f042cced20a868685491b5145b5c5db104c
Praise Jacob <iampraisejacob@gmail.com>
2025-09-06 20:17:48 +0530

    feat(c_panel): add net speed plugin

---
+++ c_panel/src/plugins/net_speed.c
@@ -0,0 +1,65 @@
+#include "colors.h"
+#include "log.h"
+#include "plugins/plugins.h"
+#include "state.h"
+#include "utils.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#define STAT_FILE "/proc/net/dev"
+#define NET_UP_ICON " "
+#define NET_DOWN_ICON " "
+#define NET_COLOR BLUE
+
+void inline static set_net_speed(PluginState* state, int down_kib, int up_kib)
+{
+    char buffer[64];
+    snprintf(buffer, sizeof(buffer), " " NET_COLOR NET_UP_ICON "%dKiB " NET_DOWN_ICON "%dKiB " RESET, up_kib, down_kib);
+    string_set_cstr(state->data, buffer);
+    panel_signal_render();
+}
+
+void static read_net_stat(int* down_kib, int* up_kib)
+{
+    FILE* file = fopen(STAT_FILE, "r");
+    *down_kib = 0;
+    *up_kib = 0;
+
+    if (!file) {
+        logger_log(LOG_DEBUG, "failed to open net dev file: " STAT_FILE);
+        return;
+    }
+
+    while (!feof(file)) {
+        char iface[32];
+        uint64_t rx_bytes, tx_bytes;
+        if (fscanf(file, "%31s %lu %*u %*u %*u %*u %*u %*u %*u %lu", iface, &rx_bytes, &tx_bytes) == 3) {
+            if (strcmp(iface, "lo:") != 0) {
+                *down_kib += rx_bytes / 1024;
+                *up_kib += tx_bytes / 1024;
+            }
+        }
+    }
+    fclose(file);
+}
+
+void* plugin_net_speed(void* _state)
+{
+    PluginState* state = _state;
+    set_net_speed(state, 0, 0);
+
+    while (running) {
+        int down1, up1, down2, up2;
+        read_net_stat(&down1, &up1);
+        msleep(5000);
+        read_net_stat(&down2, &up2);
+
+        int down_speed = down2 - down1;
+        int up_speed = up2 - up1;
+
+        set_net_speed(state, down_speed, up_speed);
+    }
+
+    return NULL;
+}

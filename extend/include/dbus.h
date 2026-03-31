ac2c5e9dae224ae217a92b1cdbb4a12816c50bc9
Praise Jacob <iampraisejacob@gmail.com>
2026-03-31 17:15:56 +0530

    refactor(extend): wrap GMainLoop with pw_loop enter and exit to run on event loop

--- extend/include/dbus.h
+++ extend/include/dbus.h
@@ -8,7 +8,7 @@
 #define IFACE   "org.freedesktop.portal.ScreenCast"
 #define REQUEST "org.freedesktop.portal.Request"
 
-void dbus_acquire_pipewire_session(GDBusConnection *conn, void (*cb)(guint32 node, gint fd));
+void dbus_acquire_pipewire_node(void (*cb)(guint32 node, gint fd));
 void dbus_free();
 
 #define define_handler_prototype(name)                                                                                 \
--- extend/src/extend.c
+++ extend/src/extend.c
@@ -3,6 +3,7 @@
 #include <gio/gio.h>
 #include <glib.h>
 #include <gst/gst.h>
+#include <pipewire/loop.h>
 #include <pipewire/pipewire.h>
 
 void
@@ -22,12 +23,20 @@ main(int argc, char **argv)
     gst_init(&argc, &argv);
     pw_init(&argc, &argv);
 
-    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    GMainLoop      *loop    = g_main_loop_new(NULL, FALSE);
+    struct pw_loop *pw_loop = pw_create_loop();
 
     dbus_acquire_pipewire_node(cb);
 
+    // event loop
+    pw_loop_enter(pw_loop);
     g_main_loop_run(loop);
+    pw_loop_leave(pw_loop);
+
+    // exit
     g_main_loop_unref(loop);
     dbus_free();
+    pw_free();
+
     return 0;
 }
